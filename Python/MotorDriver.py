77b3ce2979b9a0d3e7a195b43ddf7123847c3c05
Dan <dan.pelgrift@gmail.com>
2022-07-23 22:02:39 -0400

    Added python gcode commands

--- Python/MotorDriver.py
+++ Python/MotorDriver.py
@@ -2,11 +2,14 @@ __version__ = '0.0.1'
 
 import Config as cfg
 import time
+from numpy import pi
 from gpiozero import Servo, LED
 
 
 class MotorDriver(object):
-    def __init__(self):
+    def __init__(self, serial_device):
+
+        self.sd = serial_device
 
         self.pusher = Servo(cfg.pusherPin,min_pulse_width=cfg.pusherMinPwm,max_pulse_width=cfg.pusherMaxPwm)
         self.hopper = Servo(cfg.hopperPin,min_pulse_width=cfg.hopperMinPwm,max_pulse_width=cfg.hopperMaxPwm)
@@ -21,6 +24,55 @@ class MotorDriver(object):
         if cfg.doTurnLaserOn:
             self.laser.on()
 
+    def zero(self):
+        self.sd.command('G0 X0 Y0')
+
+    def move(self, targetYaw, targetTilt):
+        self.sd.command('G0 X{targetYaw:.2f} Y{targetYaw:.2f}')
+
+    def setYaw(self,yawDeg):
+        self.sd.command(f'G1 X{yawDeg:.2f}')
+
+    def startScan(self):
+        self.sd.command('G2')
+
+    def haltInPlace(self):
+        self.sd.command('G3')
+
+    def initMPU(self):
+        self.sd.command('M0',False)
+        ret = self.sd.waitForResponse('mpu ok',5.0)
+        return ret
+
+    def resetDMP(self):
+        self.sd.command('M1')
+
+    def configTiltServo(self):
+        self.sd.command('M2',False)
+        ret = self.sd.waitForResponse('tilt ok',15.0)
+        return ret
+
+    def setSpeedParams(self,steps_per_rev=None,yaw_speed=None,yaw_accel=None,tilt_speed=None):
+        cmd = 'M3'
+        if not steps_per_rev == None:
+            cmd += f' A{steps_per_rev}'
+        if not yaw_speed == None:
+            cmd += f' B{yaw_speed}'
+        if not yaw_accel == None:
+            cmd += f' C{yaw_accel}'
+        if not tilt_speed == None:
+            cmd += f' D{tilt_speed}'
+
+        self.sd.command(cmd)
+
+    def getCurrYawPitch(self):
+        resp = self.sd.command('M4')
+        vals = resp.split(',')
+        return float(vals[0]), float(vals[1])
+
+    def getCurrYawVel(self):
+        resp = self.sd.command('M5')
+        return float(resp)
 
     def startFiring(self):
         self.hopper.value = 1
--- Python/SerialDevice.py
+++ Python/SerialDevice.py
@@ -12,32 +12,44 @@ class SerialDevice(object):
         self.serial_dev = serial.Serial('/dev/ttyAMA0', cfg.BAUDRATE, timeout=cfg.S_TIMEOUT)
         self.R_TIMEOUT= cfg.R_TIMEOUT
 
-    def command(self, data_string):
+    def command(self, data_string,doWaitForResp=True):
         self.serial_dev.flush()
         self.serial_dev.write((data_string + '\n').encode('ascii'))
-        # tnow = time.time()
-        # last_resp = ''
-        # while time.time() - tnow < self.R_TIMEOUT:
-        #     if (self.serial_dev.inWaiting() > 0):  
-        #         resp = self.serial_dev.readline().decode('ascii')
-        #         if 'ok' in resp:
-        #             return last_resp
-        #         else:
-        #             last_resp = resp
-        #             if cfg.DEBUG_MODE:
-        #                 print('serial response:', last_resp)
-
-    def verifySerial(self,msg1,msg2):
-        self.serial_dev.flush()
-        self.serial_dev.write((msg1).encode('ascii'))
         tnow = time.time()
+        last_resp = ''
+        if not doWaitForResp:
+            return None
         while time.time() - tnow < self.R_TIMEOUT:
             if (self.serial_dev.inWaiting() > 0):  
                 resp = self.serial_dev.readline().decode('ascii')
-                if msg2 in resp:
+                if 'ok' in resp:
+                    return last_resp
+                else:
+                    last_resp = resp
+                    if cfg.DEBUG_MODE:
+                        print('serial response:', last_resp)
+
+    def verifySerial(self,msg1,msg2,timeOut=None):
+        if timeOut == None:
+            timeOut = self.R_TIMEOUT
+
+        self.serial_dev.flush()
+        self.serial_dev.write((msg1).encode('ascii'))
+
+        return self.waitForResponse(msg2,timeOut)
+
+    def waitForResponse(self,resp,timeOut=None):
+        if timeOut == None:
+            timeOut = self.R_TIMEOUT
+
+        tnow = time.time()
+        while time.time() - tnow < timeOut:
+            if (self.serial_dev.inWaiting() > 0):  
+                r = self.serial_dev.readline().decode('ascii')
+                if resp in r:
                     return True
                 else:
-                    print("Serial Response:" + resp)
+                    print("Serial Response:" + r)
         return False
 
     def readSerialLine(self):
